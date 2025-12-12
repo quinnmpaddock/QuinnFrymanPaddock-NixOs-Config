a97575caf9900f86c12da845fa3a0e05bc5870f8
Quinn <qmcconnel@gmail.com>
2025-12-11 16:28:00 -0800

    further cleanup

--- home-manager-TEMP/lazyvim/plugins/nix.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      parser_install_dir = nil,  -- Don't use custom install dir
-    },
-  },
-}
--- home-manager-TEMP/neovim/plugins/markview.lua
+++
@@ -1,6 +0,0 @@
-require("markview").setup({
-  preview = {
-    filetypes = { "markdown", "codecompanion" },
-    ignore_buftypes = {},
-  },
-})
