125bd9303e8367e4dc282d9e0509f34c82fbb780
Quinn <qmcconnel@gmail.com>
2026-01-13 16:09:12 -0800

    not even fixes at this point

--- home-manager/nixvim/config/plugins/utils/extra_plugins.nix
+++ home-manager/nixvim/config/plugins/utils/extra_plugins.nix
@@ -1,16 +1,17 @@
 { pkgs, ... }:
+let
+  nvim-nio = pkgs.vimUtils.buildVimPlugin {
+    name = "nvim-nio";
+    src = pkgs.fetchFromGitHub {
+      owner = "nvim-neotest";
+      repo = "nvim-nio";
+      rev = "a428f309119086dc78dd4b19306d2d67be884eee";
+      hash = "sha256-d7xVfM3+jOqTqV5Yf4vQJq4q8qVfM3+jOqTqV5Yf4vQ=";
+    };
+  };
+in
 {
   extraPlugins = with pkgs.vimPlugins; [
-    # nvim-nio
-    pkgs.vimUtils.buildVimPlugin
-    {
-      name = "nvim-nio";
-      src = pkgs.fetchFromGitHub {
-        owner = "nvim-neotest";
-        repo = "nvim-nio";
-        rev = "a428f309119086dc78dd4b19306d2d67be884eee";
-        hash = "sha256-d7xVfM3+jOqTqV5Yf4vQJq4q8qVfM3+jOqTqV5Yf4vQ=";
-      };
-    }
+    nvim-nio
   ];
 }
