4e576bd4e98d94b02bdded94c35b563301066906
Quinn <qmcconnel@gmail.com>
2026-04-02 08:38:19 -0700

    added opencode overlay

--- flake.nix
+++ flake.nix
@@ -16,33 +16,45 @@
     # Add LazyVim Home Manager module
     # lazyvim.url = "github:pfassina/lazyvim-nix";
     nixvim = {
-        url = "github:nix-community/nixvim";
-        # If you are not running an unstable channel of nixpkgs, select the corresponding branch of Nixvim.
-        # url = "github:nix-community/nixvim/nixos-25.11";
+      url = "github:nix-community/nixvim";
+      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of Nixvim.
+      # url = "github:nix-community/nixvim/nixos-25.11";
 
-        inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    opencode.url = "github:dan-online/opencode-nix";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, nixvim, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./hosts/nixos/configuration.nix
-        nixos-hardware.nixosModules.framework-amd-ai-300-series
-        home-manager.nixosModules.home-manager
-        # inputs.stylix.nixosModules.stylix
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-hardware,
+      home-manager,
+      nixvim,
+      opencode,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/nixos/configuration.nix
+          nixos-hardware.nixosModules.framework-amd-ai-300-series
+          home-manager.nixosModules.home-manager
+          # inputs.stylix.nixosModules.stylix
+          { nixpkgs.overlays = [ opencode.overlays.default ]; }
+          # Home Manager wiring for user quinn, import lazyvim HM module + your home.nix
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.quinn = import ./home-manager/default.nix;
 
-        # Home Manager wiring for user quinn, import lazyvim HM module + your home.nix
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; }; 
-          home-manager.users.quinn = import ./home-manager/default.nix;
-          
-        }
-      ];
+          }
+        ];
+      };
     };
-  };
 }
